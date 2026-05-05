eb41092b0e73c653a7e6d1dbdcb6ccdc9f274608
jangsan-kr <130323684+jangsan-kr@users.noreply.github.com>
2026-05-05 17:54:33 +0900

    feat: Configure base path for Vite
    Sets the `base` path in `vite.config.ts` to '/seatnumber/' to correctly deploy the application in a GitHub Pages repository.

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,16 @@
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
-import {defineConfig, loadEnv} from 'vite';
+//import {defineConfig, loadEnv} from 'vite';
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: '/seatnumber/', // <--- 저장소 이름과 동일하게 base 경로를 추가합니다.
-})
+});
 
+/*
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
@@ -30,3 +30,4 @@ export default defineConfig(({mode}) => {
     },
   };
 });
+*/
